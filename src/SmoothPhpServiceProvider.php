8d709888b5e29258695d422385d30c52347f8bb7
Simon Bennett <simon@bennett.im>
2015-11-18 14:15:21 +0000

    Add Supporting Service Provider for easer event registration

---
+++ src/SmoothPhpServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+namespace SmoothPhp\LaravelAdapter;
+
+use Illuminate\Support\ServiceProvider;
+use SmoothPhp\Contracts\EventDispatcher\EventDispatcher;
+
+/**
+ * Class SmoothPhpServiceProvider
+ * @package SmoothPhp\LaravelAdapter
+ * @author Simon Bennett <simon@bennett.im>
+ */
+abstract class SmoothPhpServiceProvider extends ServiceProvider
+{
+    /**
+     *
+     */
+    public function boot()
+    {
+        /** @var EventDispatcher $eventDispatcher */
+        $eventDispatcher = $this->app->make(EventDispatcher::class);
+
+        foreach ($this->getEventSubscribers() as $eventSubscriber) {
+            $eventDispatcher->addSubscriber($this->app->make($eventSubscriber));
+        }
+
+    }
+
+    /**
+     * @return array
+     */
+    abstract function getEventSubscribers();
+}
