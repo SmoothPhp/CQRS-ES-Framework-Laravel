27d8c9b223122508232e9ab73b6ef9da6eb485bd
Jordan Crocker <jordan@jcrocker.uk>
2018-02-09 11:21:05 +0000

    [Event Bus] Use logger helper method

--- src/EventBus/EventBusLogger.php
+++ src/EventBus/EventBusLogger.php
@@ -2,7 +2,6 @@
 
 namespace SmoothPhp\LaravelAdapter\EventBus;
 
-use Illuminate\Contracts\Logging\Log;
 use SmoothPhp\Contracts\Domain\DomainMessage;
 use SmoothPhp\Contracts\EventBus\EventListener;
 
@@ -14,19 +13,6 @@ use SmoothPhp\Contracts\EventBus\EventListener;
 final class EventBusLogger implements EventListener
 {
     /**
-     * @var Log
-     */
-    private $log;
-
-    /**
-     * @param Log $log
-     */
-    public function __construct(Log $log)
-    {
-        $this->log = $log;
-    }
-
-    /**
      * @param DomainMessage $domainMessage
      */
     public function handle(DomainMessage $domainMessage)
@@ -34,6 +20,6 @@ final class EventBusLogger implements EventListener
         $name = explode('.', $domainMessage->getType());
 
         $name = preg_replace('/(?!^)[A-Z]{2,}(?=[A-Z][a-z])|[A-Z][a-z]/', ' $0', end($name));
-        $this->log->debug(trim(ucwords($name)) . " ({$domainMessage->getType()})");
+        logger()->debug(trim(ucwords($name)) . " ({$domainMessage->getType()})");
     }
 }
